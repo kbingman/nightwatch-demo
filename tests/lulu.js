f232f8c9695cc883b7d3f38d2b6d2a6d8fa10ce0
Keith Bingman <keith@keithbingman.com>
2014-02-06 21:15:51 -0800

    eval

--- tests/lulu.js
+++ tests/lulu.js
@@ -2,18 +2,14 @@ module.exports = {
   'Demo test Lululemon' : function (browser) {
     browser
       .url('http://www.lululemon.com')
-      .waitForElementVisible('body', 1000)
-      // .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('#x-root', 10000)
+      .waitForElementVisible('body', 20000)
+      .waitForElementVisible('#x-root', 30000)
       .execute(function() {
-        var mobify = window.Mobify
-        if(window.Mobify){
-          alert('hey')
-        }
+          var evaluatedData = Mobify.evaluatedData;
 
-        return false;
+          return evaluatedData;
       }, [], function(result) {
-        console.log('result', result)
+        console.log('result', result.value.bodySubtype)
       })
       .end();
   }
